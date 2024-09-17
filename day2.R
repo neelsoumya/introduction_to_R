8ce2b84871a24d2e43c35cce114eed914167cec3
soumyabanerjee <soumyabanerjee@dhcp-10-249-11-184.eduroam.wireless.private.cam.ac.uk>
2024-09-17 10:10:47 +0100

    edits automated git

---
+++ day2.R
@@ -0,0 +1,34 @@
+
+# Installing and loading packages -----------------------------------------
+
+setwd("~/soumya_cam_mac/teaching/intro_to_R/introduction_to_R")
+library(tidyverse)
+
+# select which package required based on package name
+surveys <- read_csv(file = 'data/portal_data_joined.csv')
+
+class(surveys)
+surveys
+
+# tibble is a form of data frame in tidyverse
+
+print(surveys, n=20)
+print(surveys, n=20, width = Inf)
+
+#############
+# EXERCISE
+# print all rows how? 
+length(surveys$record_id)
+print(surveys, n=length(surveys$record_id), width = Inf)
+num_rows = length(surveys$record_id)
+print(surveys, n=num_rows, width = Inf)
+
+
+# visualization -----------------------------------------------------------
+# ggplot
+
+#ggplot(data =<DATA>, mapping=aes(<MAPPINGS>)) + <GEOM_FUNCTION>
+  
+ggplot(data = surveys, mapping = aes( x = weight, y = hindfoot_length )) + 
+  geom_point()
+
