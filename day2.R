058a9a8fc7e01f296d32c2083e9ed6b56dd1aa55
soumyabanerjee <soumyabanerjee@dhcp-10-249-11-184.eduroam.wireless.private.cam.ac.uk>
2024-09-17 10:30:08 +0100

    edits automated git

--- day2.R
+++ day2.R
@@ -47,3 +47,46 @@ ggplot(data = surveys, mapping = aes( x = weight, y = hindfoot_length )) +
   geom_point(color='blue', alpha = 0.3)
 
 
+# add a line with a smoother
+ggplot(data = surveys, mapping = aes( x = weight, y = hindfoot_length )) + 
+  geom_point() +
+  geom_smooth()
+
+####################################################
+# CONCEPT: adding functionalities layer by layer
+#   + is not addition
+#    it is adding functionality
+####################################################
+
+# change order of layering
+ggplot(data = surveys, mapping = aes( x = weight, y = hindfoot_length )) + 
+  geom_smooth() + 
+  geom_point()
+
+# points on top of line so you cannot see line
+
+
+# Concept of layering -----------------------------------------------------
+
+
+#################################
+# CONCEPT: + sign adds layers
+#     occlusion/obstruction
+#################################
+
+
+# xkcd package
+
+# save plot in variable
+
+basic_plot_surveys <- ggplot(data = surveys, mapping = aes( x = weight, y = hindfoot_length ))
+basic_plot_surveys # basic plot but no plot shown no geom given point? barplot?
+basic_plot_surveys <- basic_plot_surveys + geom_point()
+basic_plot_surveys
+
+basic_plot_surveys <- basic_plot_surveys + geom_smooth()
+basic_plot_surveys
+
+# error - will not work
+basic_plot_surveys <- basic_plot_surveys - geom_smooth()  
+
